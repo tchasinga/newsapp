169aeb5ddf5f24618fb0a6d30a06c5a208b8d55e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-03 11:43:16 +0200

    Refactor _layout.jsx for Clerk integration

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,26 +1,9 @@
 import { Stack } from "expo-router";
-import { ClerkLoaded, ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
-// import { tokenCache } from "@/lib/auth";
-import { Text, View } from "react-native";
-
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-       <ClerkLoaded>
-        <SignedIn>
           <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
           </Stack>
-        </SignedIn>
-
-        <SignedOut>
-          <View>
-          <Text>Sing out</Text>
-          </View>
-        </SignedOut>
-        </ClerkLoaded>
-    </ClerkProvider>
   );
 }
