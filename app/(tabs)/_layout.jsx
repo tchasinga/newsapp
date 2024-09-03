dafd1ef359620c20414e33efee8ee75d4750688c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-03 10:19:26 +0200

    chore: Update layout components and navigation structure

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
 const Randomlayout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+   <Tabs>
+      <Tabs.Screen name='home' options={{headerShown: false}}/>
+      <Tabs.Screen name='explore' options={{headerShown: false}}/>
+      <Tabs.Screen name='profile' options={{headerShown: false}}/>
+   </Tabs>
   )
 }
 
