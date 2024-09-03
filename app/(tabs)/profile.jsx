44b8319516fb66376078d2ae23a082bb9a33acc7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-03 10:43:20 +0200

    Refactor tab layout components and navigation structure

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function profile() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>profile</Text>
-    </View>
+    </SafeAreaView>
   )
 }
