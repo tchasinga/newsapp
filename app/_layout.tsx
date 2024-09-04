4792e35b0a5aab1a8907e6e3b1bf28090822976f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-04 12:54:53 +0200

    Refactor _layout.jsx for Clerk integration and update EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { Stack } from "expo-router";
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
 export default function RootLayout() {
 
   return (
     <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      publishableKey={publishableKey}
     >
       <ClerkLoaded>
         <Stack screenOptions={{ headerShown: false }}>
