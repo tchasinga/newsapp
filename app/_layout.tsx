aabe603f812f655400088eab85aed876492dacb1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-04 13:01:40 +0200

    Refactor _layout.jsx for Clerk integration and remove expo-secure-store dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,11 @@
 import { Stack } from "expo-router";
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
-}
 export default function RootLayout() {
 
   return (
-    <ClerkProvider
-      publishableKey={publishableKey}
-    >
-      <ClerkLoaded>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
-      </ClerkLoaded>
-    </ClerkProvider>
   );
 }
