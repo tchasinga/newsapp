507a3e7a719c12b74a81b420180d912156a4e944
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-04 12:33:30 +0200

    Refactor _layout.jsx for Clerk integration and update EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,9 +2,12 @@ import { Stack } from "expo-router";
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 
 export default function RootLayout() {
+
+  const mainLinks = "pk_test_cmVhZHktdG9tY2F0LTMuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
   return (
     <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      publishableKey={mainLinks}
     >
       <ClerkLoaded>
         <Stack screenOptions={{ headerShown: false }}>
