05014ae5d3dbfe6bf78c4f7c90c857e9060fcae1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-05 09:39:24 +0200

    new code is added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 import { Stack } from "expo-router";
-export default function RootLayout() {
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+
+export default function RootLayout() {
   return (
-        <Stack screenOptions={{ headerShown: false }}>
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+      <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
-  );
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
