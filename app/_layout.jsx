84cebb198306758810f1c0037524f4a4333bf2a8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-05 10:02:41 +0200

    new commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,18 +2,10 @@ import { ClerkProvider, ClerkLoaded , SignedIn, SignedOut} from '@clerk/clerk-ex
 import { Stack } from "expo-router";
 import { Text } from 'react-native';
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
-}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey}>
-      <ClerkLoaded>
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
        <SignedIn>
        <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -22,7 +14,6 @@ export default function RootLayout() {
        <SignedOut>
            <Text>Singout</Text>
         </SignedOut>
-      </ClerkLoaded>
     </ClerkProvider>
   )
 }
